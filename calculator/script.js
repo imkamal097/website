b0c8ac88bb00d5faaea585d3b5e476dc204c98d6
imkamal097 <143269806+imkamal097@users.noreply.github.com>
2023-10-08 06:51:56 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+function updateTextBox(value){
+    document.getElementById("screen").value += value;
+}
+function result(){
+    document.getElementById("screen").value = eval(document.getElementById("screen").value);
+}
+function reset(){
+    document.getElementById("screen").value = '';
+}
